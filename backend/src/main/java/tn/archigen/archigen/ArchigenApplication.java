8b7b54949821b47e2365d1646bfbbe7a06f1822f
Aymen Mehri <med.aymen.mehri@gmail.com>
2026-02-10 18:18:13 +0700

    generer spring boot project

---
+++ backend/src/main/java/tn/archigen/archigen/ArchigenApplication.java
@@ -0,0 +1,13 @@
+package tn.archigen.archigen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchigenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchigenApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tn/archigen/archigen/ArchigenApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.archigen.archigen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchigenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
