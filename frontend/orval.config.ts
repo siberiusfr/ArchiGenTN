906d5f832995ffcd67f7398d1ac4aeaab2d88f26
Aymen Mehri <med.aymen.mehri@gmail.com>
2026-02-11 08:10:24 +0700

    add orval

---
+++ frontend/orval.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  archigentn: {
+    input: {
+      target: "./openapi/api-docs.json",
+    },
+    output: {
+      mode: "tags-split",
+      target: "./src/api/endpoints",
+      schemas: "./src/api/models",
+      client: "react-query",
+      httpClient: "fetch",
+      baseUrl: "http://localhost:8080",
+      prettier: true,
+    },
+  },
+});
